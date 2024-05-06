56cd4b3cf3791f2d3eba667def1d1ebd09a7a231
eiken <eiken0525@gmail.com>
2024-05-07 00:05:45 +0800

    Fix some errors and complete the homework.

--- HW4/111652004_CM_HW4_P11.py
+++ HW4/111652004_CM_HW4_P11.py
@@ -1,12 +1,12 @@
 data = {1: 2.4142, 2: 2.6734, 3: 2.8974, 4: 3.0976, 5: 3.2804}
 
-R_11 = 4 * (data[1]+data[5])
+R_11 = 4 * (data[1]+data[5]) / 2
 print(f"R_11 = {R_11}")
 
-R_21 = 2 * (data[1]+data[3]+data[5])
+R_21 = 2 * (data[1]+2*data[3]+data[5]) / 2
 print(f"R_21 = {R_21}")
 
-R_31 = 1 * (data[1]+data[2]+data[3]+data[4]+data[5])
+R_31 = 1 * (data[1]+2*(data[2]+data[3]+data[4])+data[5]) / 2
 print(f"R_31 = {R_31}")
 
 R_22 = R_21 + (R_21 - R_11) / 3
