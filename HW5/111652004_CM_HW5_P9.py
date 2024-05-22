2d2b74f755f49a0be5aba2bc1ae85732b42c4a46
eiken <eiken0525@gmail.com>
2024-05-23 01:03:09 +0800

    Fix some mistakes in 5.

--- HW5/111652004_CM_HW5_P9.py
+++ HW5/111652004_CM_HW5_P9.py
@@ -32,8 +32,8 @@ def runge_kutta_fehlberg_method(f, alpha, TOL, a, b, hmax, hmin):
 
         if R <= TOL:
             t += h
-            y_0 += 25 * k_1 / 216 + 1408 * k_3 / 2565 + 2197 * k_4 / 4104 - k_5 / 5.
             t_values.append(t)
+            y_0 += 25 * k_1 / 216 + 1408 * k_3 / 2565 + 2197 * k_4 / 4104 - k_5 / 5.
             approx_soln_list.append(y_0)
 
         delta = 0.84 * pow(TOL / R, 0.25)
@@ -42,7 +42,7 @@ def runge_kutta_fehlberg_method(f, alpha, TOL, a, b, hmax, hmin):
         elif delta >= 4:
             h = 4 * h
         else:
-            h = 8 * h
+            h = delta * h
 
         if h > hmax:
             h = hmax
@@ -58,15 +58,17 @@ def runge_kutta_fehlberg_method(f, alpha, TOL, a, b, hmax, hmin):
     return t_values, approx_soln_list
 
 def f_a(t, y):
-    return y * y / (t * t) + t / t
+    return y * y / (t * t) + y / t
 
 def f_b(t, y):
     return math.sin(t) + math.exp(-t)
 
 t_values_a, approx_soln_a = runge_kutta_fehlberg_method(f_a, 1, 10 ** -4, 1, 1.2, 0.05, 0.02)
 
+print(approx_soln_a)
 plot_array(t_values_a, approx_soln_a, "Runge-Kutta-Fehlberg Method Approximated Solution: 9a")
 
 t_values_b, approx_soln_b = runge_kutta_fehlberg_method(f_b, 0, 10 ** -4, 0, 1, 0.25, 0.02)
 
+print(approx_soln_b)
 plot_array(t_values_b, approx_soln_b, "Runge-Kutta-Fehlberg Method Approximated Solution: 9b")
